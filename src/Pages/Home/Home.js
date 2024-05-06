b1926169d0c04b4b90faea99213c8d27c68d73d9
e5lam el5ayed <eslam25elsayed12@gmail.com>
2024-05-06 14:29:51 +0300

    update1

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import Drawer from '../../Components/Drawer'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <Drawer/>
+        ESlam
+    </div>
+  )
+}
