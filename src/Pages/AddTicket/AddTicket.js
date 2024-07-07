e85e27ab679e6de86920bc66e1562036f02d12af
Mariem <mariem843@gmail.com>
2024-07-07 19:28:39 +0300

    edit button

--- src/Pages/AddTicket/AddTicket.js
+++ src/Pages/AddTicket/AddTicket.js
@@ -1,36 +1,75 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Drawer from '../../Components/Drawer';
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, OutlinedInput } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Drawer from "../../Components/Drawer";
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  OutlinedInput,
+} from "@mui/material";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 export default function AddTicket() {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const [formData, setFormData] = useState({
-    name: '',
-    price: '',
-    taxes: '',
-    categoryId: '',
-    currencyId: '',
-    days: []
+    name: "",
+    price: "",
+    taxes: "",
+    categoryId: "",
+    currencyId: "",
+    days: [],
   });
   const [errors, setErrors] = useState({});
 
+  //get it=d from edit button
+  useEffect(() => {
+    const { id } = location.state || {};
+    if (id) {
+      fetchTicketDetails(id);
+    }
+  }, [location.state]);
+
+  //get ticket that will be edited
+  const fetchTicketDetails = async (id) => {
+    try {
+      const response = await axios.get(`/api/Tickets`);
+      const ticket = response.data.find((ticket) => ticket.id === id);
+      //console.log(ticket);
+      const { name, price, taxes, categoryId, currencyId } = ticket;
+      setFormData({
+        name: name,
+        price,
+        taxes,
+        categoryId,
+        currencyId,
+        days: [],
+      });
+    } catch {
+      console.log("error fetching data of ticket ");
+    }
+  };
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    if (type === 'checkbox') {
+    if (type === "checkbox") {
       const dayValue = parseInt(value); // Convert value to integer
-      setFormData(prevState => {
+      setFormData((prevState) => {
         if (checked) {
           return { ...prevState, days: [...prevState.days, dayValue] };
         } else {
-          return { ...prevState, days: prevState.days.filter(day => day !== dayValue) };
+          return {
+            ...prevState,
+            days: prevState.days.filter((day) => day !== dayValue),
+          };
         }
       });
     } else {
       setFormData({
         ...formData,
-        [name]: value
+        [name]: value,
       });
     }
   };
@@ -38,29 +77,37 @@ export default function AddTicket() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newErrors = {};
-    if (!formData.name) newErrors.name = 'من فضلك ادخل الاسم';
-    if (!formData.price) newErrors.price = ' من فضلك ادخل السعر';
-    if (!formData.taxes) newErrors.taxes = 'من فضلك ادخل الضرائب';
-    if (!formData.categoryId) newErrors.categoryId = 'من فضلك اختر نوع التذكرة';
-    if (!formData.currencyId) newErrors.currencyId = 'من فضلك اختر العملة';
-    if (formData.days.length === 0) newErrors.days = 'من فضلك اختر اليوم';
+    if (!formData.name) newErrors.name = "من فضلك ادخل الاسم";
+    if (!formData.price) newErrors.price = " من فضلك ادخل السعر";
+    if (!formData.taxes) newErrors.taxes = "من فضلك ادخل الضرائب";
+    if (!formData.categoryId) newErrors.categoryId = "من فضلك اختر نوع التذكرة";
+    if (!formData.currencyId) newErrors.currencyId = "من فضلك اختر العملة";
+    if (formData.days.length === 0) newErrors.days = "من فضلك اختر اليوم";
 
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
       return;
     }
     try {
-      const response = await axios.post('/api/Tickets', formData);
-      console.log('Ticket added successfully', response.data);
+      if (location.state && location.state.id) {
+        // Editing existing ticket
+        const response = await axios.put(
+          `/api/Tickets/${location.state.id}`,
+          formData
+        );
+        console.log("Ticket updated successfully", response);
+        localStorage.setItem("alertMessage", "تم تعديل التذكرة بنجاح");
+      } else {
+        const response = await axios.post("/api/Tickets", formData);
+        console.log("Ticket added successfully", response.data);
 
-      if (response.data) {
-        localStorage.setItem('alertMessage', 'تم إضافة التذكرة بنجاح');
+        if (response.data) {
+          localStorage.setItem("alertMessage", "تم إضافة التذكرة بنجاح");
+        }
       }
-
-      navigate('/AllTickets');
-    }
-    catch (error) {
-      console.error('There was an error adding the ticket!', error);
+      navigate("/AllTickets");
+    } catch (error) {
+      console.error("There was an error adding the ticket!", error);
     }
   };
 
@@ -69,117 +116,239 @@ export default function AddTicket() {
       <Drawer />
       <Box sx={{ width: "80%", direction: "rtl" }}>
         <div>
-          <h2 className='add-head'>اضف التذاكر</h2>
-          <Link to='/AllTickets'>
-            <button className='btn btn-primary add-button'>رجوع </button>
+          <h2 className="add-head">اضف التذاكر</h2>
+          <Link to="/AllTickets">
+            <button className="btn btn-primary add-button">رجوع </button>
           </Link>
         </div>
 
-        <div className='card table-style mb-5' style={{ direction: "rtl" }}>
+        <div className="card table-style mb-5" style={{ direction: "rtl" }}>
           <div className="card-header d-flex table-head-stayl">
             <h4>اضف البيانات</h4>
           </div>
           <div className="card-body">
             <form onSubmit={handleSubmit}>
-              <div className='container'>
-                <div className='row'>
-                  <div className='col-md-6'>
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-6">
                     <div className="form-group">
-                      <label htmlFor="name" className="d-flex">الاسم</label>
-                      <input type="text" name='name' value={formData.name} onChange={handleChange} className="form-control" id="name" />
-                      {errors.name && <h6 className="error-log">{errors.name}</h6>}
+                      <label htmlFor="name" className="d-flex">
+                        الاسم
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        className="form-control"
+                        id="name"
+                      />
+                      {errors.name && (
+                        <h6 className="error-log">{errors.name}</h6>
+                      )}
                     </div>
                   </div>
 
-                  <div className='col-md-6'>
-                    <label htmlFor="categoryId" className="d-flex">نوع التذكره</label>
-                    <select name="categoryId" value={formData.categoryId} onChange={handleChange} className="form-control">
+                  <div className="col-md-6">
+                    <label htmlFor="categoryId" className="d-flex">
+                      نوع التذكره
+                    </label>
+                    <select
+                      name="categoryId"
+                      value={formData.categoryId}
+                      onChange={handleChange}
+                      className="form-control"
+                    >
                       <option value="">اختر نوع التذكرة</option>
-                      <option value='1'>عائلي</option>
-                      <option value='2'>خاص</option>
-                      <option value='3'>فردي</option>
+                      <option value="1">عائلي</option>
+                      <option value="2">خاص</option>
+                      <option value="3">فردي</option>
                     </select>
-                    {errors.categoryId && <h6 className="error-log">{errors.categoryId}</h6>}
+                    {errors.categoryId && (
+                      <h6 className="error-log">{errors.categoryId}</h6>
+                    )}
                   </div>
 
-                  <div className='col-md-6'>
-                    <label htmlFor="price" className="d-flex">السعر</label>
+                  <div className="col-md-6">
+                    <label htmlFor="price" className="d-flex">
+                      السعر
+                    </label>
                     <FormControl fullWidth>
                       <OutlinedInput
-                        size='small'
+                        size="small"
                         id="price"
                         name="price"
                         value={formData.price}
                         onChange={handleChange}
                       />
                     </FormControl>
-                    {errors.price && <h6 className="error-log">{errors.price}</h6>}
+                    {errors.price && (
+                      <h6 className="error-log">{errors.price}</h6>
+                    )}
                   </div>
 
-                  <div className='col-md-3'>
-                    <label htmlFor="taxes" className="d-flex">الضرائب</label>
+                  <div className="col-md-3">
+                    <label htmlFor="taxes" className="d-flex">
+                      الضرائب
+                    </label>
                     <FormControl fullWidth>
                       <OutlinedInput
-                        size='small'
+                        size="small"
                         id="taxes"
                         name="taxes"
                         value={formData.taxes}
                         onChange={handleChange}
                       />
                     </FormControl>
-                    {errors.taxes && <h6 className="error-log">{errors.taxes}</h6>}
+                    {errors.taxes && (
+                      <h6 className="error-log">{errors.taxes}</h6>
+                    )}
                   </div>
 
-                  <div className='col-md-3'>
-                    <label htmlFor="currencyId" className="d-flex">العملة</label>
-                    <select name="currencyId" value={formData.currencyId} onChange={handleChange} className="form-control">
+                  <div className="col-md-3">
+                    <label htmlFor="currencyId" className="d-flex">
+                      العملة
+                    </label>
+                    <select
+                      name="currencyId"
+                      value={formData.currencyId}
+                      onChange={handleChange}
+                      className="form-control"
+                    >
                       <option value="">اختر العملة</option>
-                      <option value='1'>$</option>
-                      <option value='2'>€</option>
-                      <option value='3'>฿</option>
-                      <option value='4'>¥</option>
+                      <option value="1">$</option>
+                      <option value="2">€</option>
+                      <option value="3">฿</option>
+                      <option value="4">¥</option>
                     </select>
-                    {errors.currencyId && <h6 className="error-log">{errors.currencyId}</h6>}
+                    {errors.currencyId && (
+                      <h6 className="error-log">{errors.currencyId}</h6>
+                    )}
                   </div>
 
-                  <div className='col-md-12'>
+                  <div className="col-md-12">
                     <label className="d-flex mt-3">اختر اليوم</label>
-                    <div className='col-md-12 d-flex'>
-                      <FormGroup className='d-flex col-md-3'>
-                        <div className='col-md-2'>
-                          <FormControlLabel className='d-flex' name='days' value='1' control={<Checkbox onChange={handleChange} checked={formData.days.includes(1)} />} label="السبت" />
+                    <div className="col-md-12 d-flex">
+                      <FormGroup className="d-flex col-md-3">
+                        <div className="col-md-2">
+                          <FormControlLabel
+                            className="d-flex"
+                            name="days"
+                            value="1"
+                            control={
+                              <Checkbox
+                                onChange={handleChange}
+                                checked={formData.days.includes(1)}
+                              />
+                            }
+                            label="السبت"
+                          />
                         </div>
-                        <div className='col-md-2'>
-                          <FormControlLabel className='d-flex' name='days' value='2' control={<Checkbox onChange={handleChange} checked={formData.days.includes(2)} />} label="الاحد" />
+                        <div className="col-md-2">
+                          <FormControlLabel
+                            className="d-flex"
+                            name="days"
+                            value="2"
+                            control={
+                              <Checkbox
+                                onChange={handleChange}
+                                checked={formData.days.includes(2)}
+                              />
+                            }
+                            label="الاحد"
+                          />
                         </div>
                       </FormGroup>
-                      <FormGroup className='d-flex col-md-3'>
-                        <div className='col-md-2'>
-                          <FormControlLabel className='d-flex' name='days' value='3' control={<Checkbox onChange={handleChange} checked={formData.days.includes(3)} />} label="الاثنين" />
+                      <FormGroup className="d-flex col-md-3">
+                        <div className="col-md-2">
+                          <FormControlLabel
+                            className="d-flex"
+                            name="days"
+                            value="3"
+                            control={
+                              <Checkbox
+                                onChange={handleChange}
+                                checked={formData.days.includes(3)}
+                              />
+                            }
+                            label="الاثنين"
+                          />
                         </div>
-                        <div className='col-md-2'>
-                          <FormControlLabel className='d-flex' name='days' value='4' control={<Checkbox onChange={handleChange} checked={formData.days.includes(4)} />} label="الثلاثاء" />
+                        <div className="col-md-2">
+                          <FormControlLabel
+                            className="d-flex"
+                            name="days"
+                            value="4"
+                            control={
+                              <Checkbox
+                                onChange={handleChange}
+                                checked={formData.days.includes(4)}
+                              />
+                            }
+                            label="الثلاثاء"
+                          />
                         </div>
                       </FormGroup>
-                      <FormGroup className='d-flex col-md-3'>
-                        <div className='col-md-2'>
-                          <FormControlLabel className='d-flex' name='days' value='5' control={<Checkbox onChange={handleChange} checked={formData.days.includes(5)} />} label="الاربعاء" />
+                      <FormGroup className="d-flex col-md-3">
+                        <div className="col-md-2">
+                          <FormControlLabel
+                            className="d-flex"
+                            name="days"
+                            value="5"
+                            control={
+                              <Checkbox
+                                onChange={handleChange}
+                                checked={formData.days.includes(5)}
+                              />
+                            }
+                            label="الاربعاء"
+                          />
                         </div>
-                        <div className='col-md-2'>
-                          <FormControlLabel className='d-flex' name='days' value='6' control={<Checkbox onChange={handleChange} checked={formData.days.includes(6)} />} label="الخميس" />
+                        <div className="col-md-2">
+                          <FormControlLabel
+                            className="d-flex"
+                            name="days"
+                            value="6"
+                            control={
+                              <Checkbox
+                                onChange={handleChange}
+                                checked={formData.days.includes(6)}
+                              />
+                            }
+                            label="الخميس"
+                          />
                         </div>
                       </FormGroup>
-                      <FormGroup className='d-flex col-md-3'>
-                        <div className='col-md-2'>
-                          <FormControlLabel className='d-flex' name='days' value='7' control={<Checkbox onChange={handleChange} checked={formData.days.includes(7)} />} label="الجمعه" />
+                      <FormGroup className="d-flex col-md-3">
+                        <div className="col-md-2">
+                          <FormControlLabel
+                            className="d-flex"
+                            name="days"
+                            value="7"
+                            control={
+                              <Checkbox
+                                onChange={handleChange}
+                                checked={formData.days.includes(7)}
+                              />
+                            }
+                            label="الجمعه"
+                          />
                         </div>
                       </FormGroup>
                     </div>
-                    {errors.days && <h6 className="error-log">{errors.days}</h6>}
+                    {errors.days && (
+                      <h6 className="error-log">{errors.days}</h6>
+                    )}
                   </div>
                 </div>
               </div>
-              <button type="submit" style={{ fontSize: "20px" }} className="btn btn-primary mt-4">حفظ</button>
+              <button
+                type="submit"
+                style={{ fontSize: "20px" }}
+                className="btn btn-primary mt-4"
+              >
+                حفظ
+              </button>
             </form>
           </div>
         </div>
--- src/Pages/AddTicket/TicketsList.js
+++ src/Pages/AddTicket/TicketsList.js
@@ -1,86 +1,178 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
 export default function BasicTable() {
   const [data, setData] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get('/api/Tickets', {
+        const response = await axios.get("/api/Tickets", {
           headers: {
-            'Accept': '*/*',
-            'Content-Type': 'application/json',
-          }
+            Accept: "*/*",
+            "Content-Type": "application/json",
+          },
         });
+        console.log(response);
         setData(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     }
     fetchData();
   }, []);
 
   const dayMapping = {
-    1: 'السبت',
-    2: 'الأحد',
-    3: 'الاثنين',
-    4: 'الثلاثاء',
-    5: 'الأربعاء',
-    6: 'الخميس',
-    7: 'الجمعة'
+    1: "السبت",
+    2: "الأحد",
+    3: "الاثنين",
+    4: "الثلاثاء",
+    5: "الأربعاء",
+    6: "الخميس",
+    7: "الجمعة",
   };
 
   const categoryMapping = {
-    1: 'عائلي',
-    2: 'خاص',
-    3: 'فردي'
+    1: "عائلي",
+    2: "خاص",
+    3: "فردي",
   };
 
   const currencyMapping = {
-    1: '$',
-    2: '€',
-    3: '฿',
-    4: '¥'
+    1: "$",
+    2: "€",
+    3: "฿",
+    4: "¥",
+  };
+
+  const EditRow = (id) => {
+    console.log(id);
+    navigate(`/addTicket`, { state: { id } });
+  };
+  const DeleteRow = async (id) => {
+    console.log(id);
   };
 
   const getDayNames = (dayNumbers) => {
-    if (!Array.isArray(dayNumbers)) return '';
-    return dayNumbers.map((day) => dayMapping[day] || day).join(', ');
+    if (!Array.isArray(dayNumbers)) return "";
+    return dayNumbers.map((day) => dayMapping[day] || day).join(", ");
   };
 
   return (
-    <TableContainer className='table-style table table-hover' sx={{ direction: "rtl" }} component={Paper}>
+    <TableContainer
+      className="table-style table table-hover"
+      sx={{ direction: "rtl" }}
+      component={Paper}
+    >
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead className='table-head-stayl'>
+        <TableHead className="table-head-stayl">
           <TableRow>
-            <TableCell style={{ color: "#fff" }} sx={{ fontSize: "18px" }} align="right">الاسم</TableCell>
-            <TableCell style={{ color: "#fff" }} sx={{ fontSize: "18px" }} align="right">نوع التذكره</TableCell>
-            <TableCell style={{ color: "#fff" }} sx={{ fontSize: "18px" }} align="right">السعر</TableCell>
-            <TableCell style={{ color: "#fff" }} sx={{ fontSize: "18px" }} align="right">الضرائب</TableCell>
-            <TableCell style={{ color: "#fff" }} sx={{ fontSize: "18px" }} align="right">العملة</TableCell>
-            <TableCell style={{ color: "#fff" }} sx={{ fontSize: "18px" }} align="right">الايام</TableCell>
+            <TableCell
+              style={{ color: "#fff" }}
+              sx={{ fontSize: "18px" }}
+              align="right"
+            >
+              الاسم
+            </TableCell>
+            <TableCell
+              style={{ color: "#fff" }}
+              sx={{ fontSize: "18px" }}
+              align="right"
+            >
+              نوع التذكره
+            </TableCell>
+            <TableCell
+              style={{ color: "#fff" }}
+              sx={{ fontSize: "18px" }}
+              align="right"
+            >
+              السعر
+            </TableCell>
+            <TableCell
+              style={{ color: "#fff" }}
+              sx={{ fontSize: "18px" }}
+              align="right"
+            >
+              الضرائب
+            </TableCell>
+            <TableCell
+              style={{ color: "#fff" }}
+              sx={{ fontSize: "18px" }}
+              align="right"
+            >
+              العملة
+            </TableCell>
+            <TableCell
+              style={{ color: "#fff" }}
+              sx={{ fontSize: "18px" }}
+              align="right"
+            >
+              الايام
+            </TableCell>
+            <TableCell
+              style={{ color: "#fff" }}
+              sx={{ fontSize: "18px" }}
+              align="right"
+            >
+              الاجراءات
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.length > 0 ? data.map((ticket) => (
-            <TableRow key={ticket.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell sx={{ fontSize: "18px" }} align="right" component="th" scope="row">{ticket.name}</TableCell>
-              <TableCell sx={{ fontSize: "18px" }} align="right">{categoryMapping[ticket.categoryId]}</TableCell>
-              <TableCell sx={{ fontSize: "18px" }} align="right">{ticket.price}</TableCell>
-              <TableCell sx={{ fontSize: "18px" }} align="right">{ticket.taxes}</TableCell>
-              <TableCell sx={{ fontSize: "18px" }} align="right">{currencyMapping[ticket.currencyId]}</TableCell>
-              <TableCell sx={{ fontSize: "18px" }} align="right">{getDayNames(ticket.days)}</TableCell>
-            </TableRow>
-          )) : (
+          {data.length > 0 ? (
+            data.map((ticket) => (
+              <TableRow
+                key={ticket.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell
+                  sx={{ fontSize: "18px" }}
+                  align="right"
+                  component="th"
+                  scope="row"
+                >
+                  {ticket.name}
+                </TableCell>
+                <TableCell sx={{ fontSize: "18px" }} align="right">
+                  {categoryMapping[ticket.categoryId]}
+                </TableCell>
+                <TableCell sx={{ fontSize: "18px" }} align="right">
+                  {ticket.price}
+                </TableCell>
+                <TableCell sx={{ fontSize: "18px" }} align="right">
+                  {ticket.taxes}
+                </TableCell>
+                <TableCell sx={{ fontSize: "18px" }} align="right">
+                  {currencyMapping[ticket.currencyId]}
+                </TableCell>
+                <TableCell sx={{ fontSize: "18px" }} align="right">
+                  {getDayNames(ticket.days)}
+                </TableCell>
+                <TableCell sx={{ fontSize: "18px" }} align="right">
+                  <button
+                    className="btn btn-primary ml-2"
+                    onClick={() => EditRow(ticket.id)}
+                  >
+                    تعديل
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => DeleteRow(ticket.id)}
+                  >
+                    حذف
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
             <TableRow>
               <TableCell colSpan={6} align="center">
                 {"No data available"}
