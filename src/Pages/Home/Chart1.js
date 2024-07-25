1aada6c96dc1daa7589da6d96827b0ddeee517ef
Mariem <mariem843@gmail.com>
2024-07-25 15:33:36 +0300

    new update

--- src/Pages/Home/Chart1.js
+++ src/Pages/Home/Chart1.js
@@ -1,66 +1,21 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+import * as React from "react";
+import { PieChart } from "@mui/x-charts/PieChart";
 
-class ApexChart extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            series: [{
-                name: "التأخير",
-                data: [10, 41, 35, 51,69, 91, 148]
-            },
-            {
-                name: "الغياب",
-                data: [ 99, 30, 60, 49, 10, 100, 69]
-            },
-            {
-                name: "الحضور",
-                data: [70, 49, 44, 67, 101, 94, 30]
-            }],
-            options: {
-                chart: {
-                    height: 350,
-                    type: 'line',
-                    zoom: {
-                        enabled: false
-                    }
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                stroke: {
-                    curve: 'straight'
-                },
-                title: {
-                    text: '',
-                    align: 'left'
-                },
-                grid: {
-                    row: {
-                        colors: ['#f3f3f3', 'transparent'],
-                        opacity: 0.5
-                    },
-                },
-                xaxis: {
-                    categories: ['السبت', 'الاحد', 'الاثنين', 'الثلاثاء', 'الاربعاء', 'الخميس', 'الجمعه'],
-                }
-            }
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                
-                        <div id="chart">
-                            <ReactApexChart options={this.state.options} series={this.state.series} type="line" height={350} />
-                        </div>
-                        <div id="html-dist"></div>
-                   
-            </div>
-        );
-    }
+export default function BasicPie() {
+  return (
+    <PieChart
+      colors={["#FFD1DC", "#FFDEAD", "#E0BBE4", "#C6E2FF"]}
+      series={[
+        {
+          data: [
+            { id: 0, value: 10, label: "عائليه" },
+            { id: 1, value: 15, label: "ضيافه" },
+            { id: 2, value: 20, label: "تذكره الجمعه " },
+            { id: 3, value: 20, label: "كبير" },
+          ],
+        },
+      ]}
+      height={240}
+    />
+  );
 }
-
-export default ApexChart;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,45 +1,190 @@
-import React from 'react'
-import Drawer from '../../Components/Drawer';
-import { Box} from '@mui/material';
-import Chart1 from './Chart1';
-import Chart2 from './Chart2';
-import Chart3 from './Chart3';
-import Chart4 from './Chart4';
-import { Grid } from '@mui/material';
-import Row1 from './Row1';
+import React, { useState } from "react";
+import SalesBarChart from "./SaleBarChart";
+import Drawer from "../../Components/Drawer";
+import Paper from "@mui/material/Paper";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import FamilyRestroomIcon from "@mui/icons-material/FamilyRestroom";
+import PeopleIcon from "@mui/icons-material/People";
+import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import BasicPie from "./Chart1";
+const Home = () => {
+  const [startDate, setStartDate] = useState("2023-05-01");
+  const [endDate, setEndDate] = useState("2024-07-21");
 
+  const theme = useTheme();
+  const isMobileOrMedium = useMediaQuery(theme.breakpoints.down("md"));
 
-export default function home() {
+  const data = {
+    labels: [
+      "01-05-2023",
+      "02-05-2023",
+      "03-05-2023",
+      "04-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "01-05-2023",
+      "02-05-2023",
+      "03-05-2023",
+      "04-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      "05-05-2023",
+      // Add more dates as needed
+    ],
+    datasets: [
+      {
+        label: "Tickets Sold",
+        data: [
+          20, 40, 30, 50, 60, 70, 80, 90, 100, 20, 40, 30, 50, 60, 70, 80, 90,
+          100, 20, 40, 30, 50, 60, 70, 80, 90, 100,
+        ], // Add more ticket data as needed
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const handleStartDateChange = (e) => setStartDate(e.target.value);
+  const handleEndDateChange = (e) => setEndDate(e.target.value);
+
+  const pastelColors = ["#FFD1DC", "#FFDEAD", "#E0BBE4", "#C6E2FF"];
+  const paperData = [
+    { label: "عائليه", count: 26, icon: <FamilyRestroomIcon /> },
+    { label: "ضيافه", count: 49, icon: <PeopleIcon /> },
+    { label: "تذكره الجمعه", count: 49, icon: <ConfirmationNumberIcon /> },
+    { label: "كبير", count: 41, icon: <AccessibilityIcon /> },
+  ];
+
   return (
-    <div>
+    <>
       <Drawer />
-     
-      <Box sx={{ width: "78%", ml:"50px",mr:"auto", marginTop: "-20px",marginBottom:"3%" }}>
+      <div
+        style={{
+          padding: "0 60px",
+          fontFamily: "Arial, sans-serif",
+          marginRight: isMobileOrMedium ? "0" : "240px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          position: "relative",
+        }}
+      >
+        <h3>Today's Sales</h3>
 
-      <Row1 />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: isMobileOrMedium ? "center" : "space-between",
+            flexDirection: isMobileOrMedium ? "column" : "row",
+            marginTop: "10px",
+          }}
+        >
+          {paperData.map((item, index) => (
+            <Paper
+              key={index}
+              style={{
+                textAlign: "center",
+                backgroundColor: pastelColors[index % pastelColors.length],
+                padding: "10px",
+                borderRadius: "15px",
+                width: isMobileOrMedium ? "100%" : "20%",
+                marginBottom: isMobileOrMedium ? "10px" : "0",
+                transition: "background-color 0.3s ease",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+              elevation={2}
+              onMouseEnter={(e) =>
+                (e.currentTarget.style.backgroundColor = "#FFE4E1")
+              }
+              onMouseLeave={(e) =>
+                (e.currentTarget.style.backgroundColor =
+                  pastelColors[index % pastelColors.length])
+              }
+            >
+              <>{item.icon}</>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <h2>{item.label}</h2>
+                <p>{item.count}</p>
+              </div>
+            </Paper>
+          ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            marginTop: "20px",
+            flexDirection: isMobileOrMedium ? "column" : "row",
+          }}
+        >
+          <label style={{ width: isMobileOrMedium ? "100%" : "50%" }}>
+            From:
+            <input
+              type="date"
+              value={startDate}
+              onChange={handleStartDateChange}
+              style={{
+                width: "100%",
+                height: "40px",
+                borderRadius: "15px",
+                border: "1px solid grey",
+                padding: "20px",
+                marginBottom: isMobileOrMedium ? "10px" : "0",
+              }}
+            />
+          </label>
+          <label
+            style={{
+              width: isMobileOrMedium ? "100%" : "50%",
+              marginLeft: isMobileOrMedium ? "0" : "10px",
+            }}
+          >
+            To:
+            <input
+              type="date"
+              value={endDate}
+              onChange={handleEndDateChange}
+              style={{
+                width: "100%",
+                height: "40px",
+                borderRadius: "15px",
+                border: "1px solid grey",
+                padding: "20px",
+              }}
+            />
+          </label>
+        </div>
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            width: "100%",
+            alignItems: "center",
+            flexDirection: isMobileOrMedium ? "column" : "row",
+            heigh: "100%",
+          }}
+        >
+          <SalesBarChart data={data} />
+          <BasicPie />
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-        <Grid container spacing={1} mt={2} display={"flex"} justifyContent={"space-between"}>
-          <Grid className='graid-chart' item md={6} xs={12} sx={{ backgroundColor: "#fff",marginLeft:"-10px" }}>
-            <Chart1 />
-          </Grid>
-          <Grid item md={6} xs={12} sx={{ backgroundColor: "#fff",marginRight:"-10px" }}>
-            <Chart2 />
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={1} mt={3} display={"flex"} justifyContent={"space-between"}>
-          <Grid item md={6} xs={12} sx={{ backgroundColor: "#fff" , display:"flex",alignItems:"center",justifyContent:"center",marginLeft:"-10px"}}>
-            <Chart4 />
-          </Grid>
-          <Grid item md={6} xs={12} sx={{ backgroundColor: "#fff",marginRight:"-10px" }}>
-            <Chart3 />
-          </Grid>
-        </Grid>
-
-
-      </Box>
-
-    </div>
-  )
-}
+export default Home;
