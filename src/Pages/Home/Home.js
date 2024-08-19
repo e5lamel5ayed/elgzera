059638c33d13115b45328678806ffd9bc3de55fe
sayedosama7 <sayedosama088@gmail.com>
2024-08-19 16:46:49 +0300

    home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,10 +4,10 @@ import Drawer from "../../Components/Drawer";
 import Paper from "@mui/material/Paper";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-// import FamilyRestroomIcon from "@mui/icons-material/FamilyRestroom";
-// import PeopleIcon from "@mui/icons-material/People";
-// import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
-// import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import FamilyRestroomIcon from "@mui/icons-material/FamilyRestroom";
+import PeopleIcon from "@mui/icons-material/People";
+import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
 import BasicPie from "./Chart1";
 import axios from "axios";
 import { baseURL, TOTAL_DAILY_REPORTS } from "../../Components/Api";
@@ -35,20 +35,20 @@ const Home = () => {
   const theme = useTheme();
   const isMobileOrMedium = useMediaQuery(theme.breakpoints.down("md"));
 
-  // const getCategoryIcon = (index) => {
-  //   switch (index % 4) {
-  //     case 0:
-  //       return <FamilyRestroomIcon />;
-  //     case 1:
-  //       return <PeopleIcon />;
-  //     case 2:
-  //       return <ConfirmationNumberIcon />;
-  //     case 3:
-  //       return <AccessibilityIcon />;
-  //     default:
-  //       return <PeopleIcon />;
-  //   }
-  // };
+  const getCategoryIcon = (index) => {
+    switch (index % 4) {
+      case 0:
+        return <FamilyRestroomIcon />;
+      case 1:
+        return <PeopleIcon />;
+      case 2:
+        return <ConfirmationNumberIcon />;
+      case 3:
+        return <AccessibilityIcon />;
+      default:
+        return <PeopleIcon />;
+    }
+  };
 
   // fetch data for today 
   useEffect(() => {
@@ -120,50 +120,53 @@ const Home = () => {
           position: "relative",
         }}
       >
-        <div className="card-header d-flex table-head-style justify-content-end">
-          <h3>مبيعات اليوم</h3>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: isMobileOrMedium ? "center" : "space-between",
-            flexDirection: isMobileOrMedium ? "column" : "row",
-            marginTop: "10px",
-          }}
-        >
-          {all.map((item, index) => (
-            <Paper
-              key={index}
-              style={{
-                textAlign: "center",
-                backgroundColor: pastelColors[index % pastelColors.length],
-                padding: "10px",
-                borderRadius: "15px",
-                width: isMobileOrMedium ? "100%" : "20%",
-                marginBottom: isMobileOrMedium ? "10px" : "0",
-                transition: "background-color 0.3s ease",
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-around",
-                margin: '5px'
-              }}
-              elevation={2}
-              onMouseEnter={(e) =>
-                (e.currentTarget.style.backgroundColor = "#FFE4E1")
-              }
-              onMouseLeave={(e) =>
-              (e.currentTarget.style.backgroundColor =
-                pastelColors[index % pastelColors.length])
-              }
-            >
-              <>{item.icon}</>
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <h2>{item.category}</h2>
-                <h2>{item.quantity}</h2>
-              </div>
-            </Paper>
-          ))}
+        <div>
+          <div className="card-header d-flex table-head-style justify-content-end">
+            <h3>مبيعات اليوم</h3>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: isMobileOrMedium ? "center" : "space-between",
+              flexDirection: isMobileOrMedium ? "column" : "row",
+              marginTop: "10px",
+            }}
+          >
+            {all.map((item, index) => (
+              <Paper
+                key={index}
+                style={{
+                  textAlign: "center",
+                  backgroundColor: pastelColors[index % pastelColors.length],
+                  padding: "10px",
+                  borderRadius: "15px",
+                  width: isMobileOrMedium ? "100%" : "20%",
+                  marginBottom: isMobileOrMedium ? "10px" : "0",
+                  transition: "background-color 0.3s ease",
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                  margin: '5px'
+                }}
+                elevation={2}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.backgroundColor = "#FFE4E1")
+                }
+                onMouseLeave={(e) =>
+                (e.currentTarget.style.backgroundColor =
+                  pastelColors[index % pastelColors.length])
+                }
+              >
+                {/* show icon with category*/}
+                {getCategoryIcon(index)}
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <h3>{item.category}</h3>
+                  <h5>{item.quantity}</h5>
+                </div>
+              </Paper>
+            ))}
+          </div>
         </div>
 
         <div
